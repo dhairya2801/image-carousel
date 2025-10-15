2c47064c5f792ff1e3010cc687776631355161b3
Dhairya <dhairyashah.ai@gmail.com>
2025-10-15 01:27:27 -0400

    Solved carousel technique mistake
    Earlier, I was merging two ideas of
    implementing the carousel. 1. Used
    translateX in js to move the parent
    slides container. 2. used toggle .active
    class to set the active slide. Now, I
    only use the first approach, i.e. move
    the slides container horizontally upon
    button click.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const nextBtn = document.querySelector('#next');
 const prevBtn = document.querySelector('#prev');
 const slides = document.querySelector('#slides');
 const pictureFrame = document.querySelector('#picture-frame');
+const btns = document.querySelector('#btns');
 
 let x = 0;
-slides.children[x].classList.add('active');
 
-pictureFrame.addEventListener('click', (e) => {
+btns.addEventListener('click', (e) => {
     if (e.target.id === 'next' || e.target.id === 'prev') {
         move(e.target.id);
     }
@@ -19,5 +19,5 @@ function move(dir) {
     else {
         x = (x-1+6)%6;
     }
-    slides.style.transform = `translateX(-${(x)*50}vw)`;
+    slides.style.transform = `translateX(-${(x)*100}%)`;
 }
