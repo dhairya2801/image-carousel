b9014d039d9083cb1971a89d4b232e1d9fbe7178
Dhairya <dhairyashah.ai@gmail.com>
2025-10-15 03:12:09 -0400

    Added and linked nav-btns to slides

--- script.js
+++ script.js
@@ -5,19 +5,26 @@ const pictureFrame = document.querySelector('#picture-frame');
 const btns = document.querySelector('#btns');
 
 let x = 0;
+btns.children[x].classList.add('active');
+setInterval(() => move(), 5000);
 
 btns.addEventListener('click', (e) => {
-    if (e.target.id === 'next' || e.target.id === 'prev') {
-        move(e.target.id);
+    if (e.target.classList.contains('nav-btn')) {
+        btns.children[x].classList.remove('active');
+        i = Array.from(btns.children).indexOf(e.target);
+        goTo(i);
     }
-})
+}) 
 
-function move(dir) {
-    if (dir === 'next') {
-        x = (x+1)%6;
-    }
-    else {
-        x = (x-1+6)%6;
-    }
+function goTo(i) {
+    btns.children[x].classList.remove('active');
+    
+    x = i;
+
     slides.style.transform = `translateX(-${(x)*100}%)`;
+    btns.children[x].classList.add('active');
+}
+
+function move() {
+    goTo((x+1)%6);
 }
